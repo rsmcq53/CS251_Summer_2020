0d43d76a71a052ed3e53ce604cb3db65570b7a73
Carol Brackman <carolbrackman@gmail.com>
2020-07-10 13:36:54 -0700

    Submission for Module 1

---
+++ ON/CarolBrackman/Module1/simpleStat.R
@@ -0,0 +1,8 @@
+#Calculate the standard deviation of vector of numbers
+print(sd(c(1,2,3,4,5,6)))
+
+#Calculate the mean of a vector of numbers
+print(mean(c(1,2,3,4,5,6)))
+
+#Calculate the minimum number
+print(min(c(1,2,3,4,5,6)))
