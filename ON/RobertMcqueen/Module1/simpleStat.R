7be015416cb98d70ff7b88852165813e81479120
B53 <67708075+rsmcq53@users.noreply.github.com>
2020-07-13 01:13:47 -0700

    HOP01_RobertMcqueen
    HOP01 first commit.

---
+++ ON/RobertMcqueen/Module1/simpleStat.R
@@ -0,0 +1,8 @@
+#Calculate the standard deviation of vector numbers
+print(sd(c(1,2,3,4,5,6)));
+
+#Calculate the mean of a vecctor of numbers
+print(mean(c(1,2,3,4,5,6)));
+
+#Calculate the minimum number
+print(min(c(1,2,3,4,5,6)));
